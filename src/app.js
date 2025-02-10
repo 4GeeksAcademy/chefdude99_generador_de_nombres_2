85845469cf01da9e2ddb44c2d46dd9acbdcee076
chefdude99 <chefdude.1999@gmail.com>
2025-02-10 23:00:52 +0000

    proyecto listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let final = ['.com', '.yo', '.tengohambre']
 
+for (let i = 0; i < pronoun.length; i++) {
+  for (let u = 0; u < adj.length; u++) {
+    for (let p = 0; p < noun.length; p++) {
+      for (let o = 0; o < final.length; o++)
+      console.log(pronoun[i]+adj[u]+noun[p]+final[o])
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
